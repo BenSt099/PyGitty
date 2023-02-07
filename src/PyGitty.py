1d0fb40327415eb9c89a65a9968a2336f23af68d
BenSt099 <ben.steinhauer.de@gmail.com>
2023-02-07 22:06:36 +0100

    updated pygitty #2

--- src/PyGitty.py
+++ src/PyGitty.py
@@ -1,11 +1,18 @@
 import argparse
 import os
+import sys
 import json
 from pathlib import Path
 from FileSystemScanner import get_files_with_path
 
 def printVersion():
     print("PyGitty - V1.0.0")
+
+def addToCommit():
+    print()
+
+def commitToRepo():    
+    print()
 
 def checkStatus():
     print("PiGitty - Status")
@@ -15,6 +22,8 @@ def checkStatus():
     for files in files_dict:
         os.path.getmtime(files_dict[files])
 
+    #if not os.path.exists(os.path.join(os.getcwd(),"pygittyconfig")):
+    #    print("The following files were modified")
 
 
 
@@ -22,16 +31,21 @@ def checkStatus():
 def initRepo():
     if os.getcwd().endswith(".pygitty"):
 
-        path_To_JSON = os.path.join(os.getcwd(),"pygittyconfig")
+        path_To_JSON = os.path.join(os.getcwd(),"pygittyconfig.json")
 
         if not os.path.exists(path_To_JSON):
             with open(path_To_JSON, 'w') as json_file:
                 json.dump({}, json_file)
     
-        print("✅ Initialisatio Successful")
+        print("✅ Initialisation Successful")
     else:
         print("❌ Not A PyGitty Repository")
 
+
+def isRepoInRightCondition():
+    if os.getcwd().endswith(".pygitty"):
+        return False
+    return True    
 
 def writeToJSONConfigFile(input):
     with open("pygittyconfig.json", "w+") as outfile:
@@ -47,18 +61,25 @@ def inputArgs():
     parser.add_argument('--status','-status', dest='status',action='store_true', help='Displays Current Status of PyGitty')
     parser.add_argument('--add','-add', dest='add',action='store_true', help='Adds file to a commit')
     parser.add_argument('--commit','-commit', dest='commit',action='store_true', help='Finishes Commit')
-    parser.add_argument('--init','-init', dest='init',action='store_true', help='Initialises a pygitty repository')
+    parser.add_argument('--init','-init', dest='init',action='store_true', help='Initialises a Pygitty repository')
     args = parser.parse_args()
 
     if args.printVersion:
        printVersion()
-    if args.status:
+    elif args.status:
        checkStatus()
-    if args.init:
+    elif args.init:
        initRepo()
+    elif args.add:
+        addToCommit()
+    elif args.commit:
+        commitToRepo()
+    else:
+        print("PyGitty - Wrong Parameter")    
 
-
-def startPiGitty():
+def startPyGitty():
+    if isRepoInRightCondition == False:
+        sys.exit("❌ Wrong Conditions in Repository")
     inputArgs()
 
-startPiGitty()
+startPyGitty()
